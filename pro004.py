df0cd60180e54fa6421ae0f5b07edc211fb7b522
Teddy <i.kenting@gmail.com>
2014-12-26 21:34:22 +0800

    problem 4 solved, find the largest palindrome product.

---
+++ pro004.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# _*_ coding: utf-8 _*_
+
+
+
+def is_palindrome_num(n):
+    if str(n) == str(n)[::-1]:
+        return True
+    else:
+        return False
+
+
+def largest_palindrome(start, end):
+    max_palindrome = 9009
+    for i in range(start, end+1):
+        for j in range(start, end+1):
+            tmp = i * j
+            if is_palindrome_num(tmp) and tmp > max_palindrome:
+                max_palindrome = tmp
+    return max_palindrome
+
+
+if __name__ == '__main__':
+    print largest_palindrome(100, 999)
