8d256b11872d71e6fc63e77ed8be1b7b25a9a072
saturnisbig <i.kenting@gmail.com>
2015-01-26 14:18:38 +0800

    Problem 22 names scores, sort name from a file and cal value solved

--- README.md
+++ README.md
@@ -3,4 +3,4 @@ euler-python
 
 Project Euler solutions by Python
 
-15-21-22-19-48-24-28-30-29-67-23
+15-21)-22)-19-48-24-28-30-29-67-23
--- pro022.py
+++ pro022.py
@@ -15,7 +15,38 @@ obtain a score of 938 × 53 = 49714.
 
 What is the total of all the name scores in the file?
 
-Costs: 
+Costs: 2015-01-26 08:53:40 - 2015-01-26 14:17:08
 """
 
 
+def first_name_sorted_alph():
+    with open('p022_names.txt', 'r') as fh:
+        for line in fh:
+            tmp = line.split(',')
+    result = [eval(s) for s in tmp]
+    return sorted(result)
+
+
+def name_scores(name, pos):
+    # A -> 1, B -> 2, start from 1
+    intA = ord('A') - 1
+    score = map(ord, name)
+    score = [i-intA for i in score]
+    return sum(score) * pos
+
+def names_scores(l):
+    result = 0
+    for i, name in enumerate(l):
+        score = name_scores(name, i+1)
+        result += score
+        #if name == 'COLIN':
+        #    print i+1, name
+        #    break
+    return result
+
+
+if __name__ == '__main__':
+    print first_name_sorted_alph()[0]
+    l = first_name_sorted_alph()
+    print name_scores('COLIN', 938)
+    print names_scores(l)
