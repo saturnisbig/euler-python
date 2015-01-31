2701d5484774499ea015f4690bb3403d511c0390
Teddy <i.kenting@gmail.com>
2015-01-31 12:54:46 +0800

    Problem 48 self power sums solved

--- pro048.py
+++ pro048.py
@@ -7,7 +7,16 @@ The series, 1^1 + 2^2 + 3^3 + ... + 10^10 = 10405071317.
 
 Find the last ten digits of the series, 1^1 + 2^2 + 3^3 + ... + 1000^1000.
 
-Costs: 
+Costs: 2015-01-31 12:48:54 - 2015-01-31 12:52:43
 """
 
 
+def get_sum_of_self_power(n):
+    result = 0
+    for i in xrange(1, n+1):
+        result += i**i
+    return result
+
+
+if __name__ == '__main__':
+    print str(get_sum_of_self_power(1000))[-10:]
