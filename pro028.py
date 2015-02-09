467fdba3d6dc9398f13349b6950be1efd6a31430
Teddy <i.kenting@gmail.com>
2015-02-09 23:02:21 +0800

    Problem 28 Number spiral diagnoals solved.

--- pro028.py
+++ pro028.py
@@ -13,7 +13,44 @@ by 5 spiral is formed as follows:
 It can be verified that the sum of the numbers on the diagonals is 101.
 What is the sum of the numbers on the diagonals in a 1001 by 1001 spiral formed in the same way?
 
-Costs: 
+Costs: 2015-02-09 22:40:43 - 2015-02-09 22:56:14
+思路：其实就是发现了以下规律，1;1+2,1+2+2,1+2+2+2,1+2+2+2+2; 9+1*4,9+2*4...
+每次都是四个数，按2-4-6-8递增。
 """
 
 
+def get_number_spiral(n):
+    result = [1]
+    counter = 1
+    starter = 2
+    while True:
+        if counter > n:
+            break
+        for j in range(4):
+            counter += starter
+            if counter > n:
+                break
+            result.append(counter)
+        starter += 2
+    return result
+
+def get_number_spiral_iter(n):
+    counter = 1
+    starter = 2
+    yield counter
+    while True:
+        if counter > n:
+            break
+        for i in range(4):
+            counter += starter
+            if counter > n:
+                break
+            yield counter
+        starter += 2
+
+def find_result():
+    print sum(get_number_spiral_iter(1001*1001))
+
+if __name__ == '__main__':
+    print sum(get_number_spiral(1001*1001))
+    find_result()
