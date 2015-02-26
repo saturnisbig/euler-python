bf00ee6e1206e89187449e59c261af2e2a067966
Teddy <i.kenting@gmail.com>
2015-02-26 21:47:53 +0800

    Fix the bug in the function, and add doctest for get_cycle() func.

--- pro026.py
+++ pro026.py
@@ -12,29 +12,52 @@ in its decimal fraction part.
 Costs: 2015-02-26 19:12:06 - 2015-02-26 19:42:51
 """
 
-def get_result(n):
+def get_cycle(n):
+    """
+    >>> get_cycle(3)
+    (3, 1)
+    >>> get_cycle(6)
+    (6, 1)
+    >>> get_cycle(7)
+    (7, 6)
+    >>> get_cycle(17)
+    (17, 16)
+    >>> get_cycle(81)
+    (81, 9)
+    >>> get_cycle(2)
+    (2, 0)
+    >>> get_cycle(9)
+    (9, 1)
+    """
+    power = 10**len(str(n))
+    starter = 1 * power
+    tmp1 = []
+    counter = 0
+    while True:
+        r = starter % n
+        if r == 0:
+            break
+        if r not in tmp1:
+            tmp1.append(r)
+            counter += 1
+            # 将余数乘10，按照除法的法则
+            starter = r * 10
+        else:
+            cycle_start = tmp1.index(r)
+            if cycle_start > 0:
+                counter = len(tmp1) - cycle_start - 1
+            break
+    return n, counter
+
+
+def get_longest_cycle(n):
     result = (0, 0)
     for i in xrange(2, n):
-        power = 10**len(str(i))
-        starter = 1 * power
-        tmp1 = []
-        counter = 0
-        while True:
-            r = starter % i
-            if r == 0:
-                break
-            #d = int(starter / i)
-            if r not in tmp1:
-                tmp1.append(r)
-                starter = r * power
-                counter += 1
-            else:
-                break
-            #print i, r
-        if counter > result[0]:
-            result = (counter, i)
+        tmp = get_cycle(i)
+        if tmp[1] > result[1]:
+            result = tmp
     return result
 
 
 if __name__ == '__main__':
-    print get_result(1000)
+    print get_longest_cycle(1000)
